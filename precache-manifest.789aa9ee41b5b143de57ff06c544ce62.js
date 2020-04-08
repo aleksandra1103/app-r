f42dd81325734d74ab1345588c8fd9cd8614991c
Aleksandra Franczak <aleksandra.franczak1989@gmail.com>
2020-04-08 11:55:20 +0200

    Updates

---
+++ precache-manifest.789aa9ee41b5b143de57ff06c544ce62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff98d76c6d3b7c08ede7575709762d2d",
+    "url": "/app-r/index.html"
+  },
+  {
+    "revision": "2304d9da61c80cce9a6c",
+    "url": "/app-r/static/css/main.0b15e01c.chunk.css"
+  },
+  {
+    "revision": "42e299326376a3d8c133",
+    "url": "/app-r/static/js/2.309c64af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-r/static/js/2.309c64af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2304d9da61c80cce9a6c",
+    "url": "/app-r/static/js/main.4686db90.chunk.js"
+  },
+  {
+    "revision": "8e624dc1441b70d8e080",
+    "url": "/app-r/static/js/runtime-main.368c7ea6.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-r/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-r/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-r/static/media/header3.d9d36b65.jpg"
+  }
+]);
